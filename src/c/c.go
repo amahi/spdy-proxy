52b8a1c7f31dfcb8a26430215298b975cf68d02e
Jamie Hall <the.sly.marbo@googlemail.com>
2013-09-17 11:37:26 +0100

    Update c.go
    Ensured reconnection after disconnect from proxy.

--- src/c/c.go
+++ src/c/c.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -9,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httputil"
+	"time"
 )
 
 func handle(err error) {
@@ -21,34 +21,58 @@ const HOST_PORT = "localhost:1444"
 const DIR_TO_SERVE = "./media"
 
 func main() {
-	// connect to P
-	conn, err := tls.Dial("tcp", HOST_PORT, &tls.Config{InsecureSkipVerify: true})
-	handle(err)
-
-	// build the request
-	buf := new(bytes.Buffer)
-	_, err = buf.WriteString("Hello from C")
-	handle(err)
-	req, err := http.NewRequest("PUT", "https://"+HOST_PORT, buf)
-	handle(err)
-
-	// make the client connection
-	client := httputil.NewClientConn(conn, nil)
-	res, err := client.Do(req)
-	handle(err)
-	buf.Reset()
-	_, err = io.Copy(buf, res.Body)
-	handle(err)
-	fmt.Printf("%q from P: %q.\n", res.Status, buf.String())
-
-	// swap
-	//spdy.EnableDebugOutput()
-	c, _ := client.Hijack()
-	conn = c.(*tls.Conn)
-	srv := &http.Server{Handler: http.FileServer(http.Dir(DIR_TO_SERVE))}
-	spdy.AddSPDY(srv)
-	server, err := spdy.NewServerConn(conn, srv, 3)
-	handle(err)
-	fmt.Println("Ready")
-	handle(server.Run())
+	for {
+		var conn *tls.Conn
+		var err error
+		for i := 0; i < 10; i++ {
+			// connect to P.
+			conn, err := tls.Dial("tcp", HOST_PORT, &tls.Config{InsecureSkipVerify: true})
+			if err != nil {
+				time.Sleep(100 * time.Millisecond)
+			} else {
+				break
+			}
+		}
+		if conn == nil {
+			fmt.Println("Failed to connect. Waiting 30 seconds.")
+			time.Sleep(30 * time.Second)
+			continue
+		}
+	
+		// build the request
+		buf := new(bytes.Buffer)
+		_, err = buf.WriteString("Hello from C")
+		handle(err)
+		req, err := http.NewRequest("PUT", "https://"+HOST_PORT, buf)
+		handle(err)
+	
+		// make the client connection
+		client := httputil.NewClientConn(conn, nil)
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Error: Failed to make connection to P:", err)
+			continue
+		}
+		buf.Reset()
+		_, err = io.Copy(buf, res.Body)
+		handle(err)
+		fmt.Printf("%q from P: %q.\n", res.Status, buf.String())
+	
+		// swap
+		//spdy.EnableDebugOutput()
+		c, _ := client.Hijack()
+		conn = c.(*tls.Conn)
+		srv := &http.Server{Handler: http.FileServer(http.Dir(DIR_TO_SERVE))}
+		spdy.AddSPDY(srv)
+		server, err := spdy.NewServerConn(conn, srv, 3)
+		if err != nil {
+			fmt.Println("Encountered error creating SPDY server connection:", err)
+			continue
+		}
+		fmt.Println("Ready")
+		err = server.Run()
+		if err != nil {
+			fmt.Println("Encountered error serving:", err, "\nReconnecting to proxy...")
+		}
+	}
 }
