4eb30757d94def87eda75bb94646d49b24f1d2a3
Carlos Puchol <cpg+git@amahi.org>
2013-09-28 00:57:06 -0700

    sample api client to test the server with slow connections

---
+++ sample-client/sample-client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+const BUFFER_SIZE = 256
+
+func handle(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	resp, err := http.Get("http://localhost:1443/sample.avi")
+	handle(err)
+	defer resp.Body.Close()
+
+	f, err := os.OpenFile("/tmp/outfile", os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0644)
+	handle(err)
+
+	copied := int64(0)
+	for {
+		written, err := io.CopyN(f, resp.Body, BUFFER_SIZE)
+		copied += written
+		if err != nil {
+			// EOF -- ignore the rest
+			break
+		}
+		time.Sleep(200 * time.Millisecond)
+	}
+
+	fmt.Printf("Received: %d bytes\n", copied)
+}
+
